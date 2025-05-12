bd0367e88bbf6aa07f364ad62ec50ec45ba62b66
adva-davidof <adva.davidof@gmail.com>
2025-05-12 08:29:35 +0300

    d

--- forme.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
-import numpy as np
-import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader, random_split
-
-print(transforms)
